51311a0f858967f2643dfcee610cc88ee9cb5500
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-01-21 04:16:04 -0800

    Initial Commit

---
+++ src/main/java/dev/sumantakumar/singlecommand/CompileAndRunWithOneCode.java
@@ -0,0 +1,20 @@
+package dev.sumantakumar.singlecommand;
+
+class A{
+    public static void main(String[] args) {
+        System.out.println("A - main()");
+    }
+}
+
+class C{
+    public static void main(String[] args) {
+        System.out.println("C - main()");
+    }
+}
+
+
+public class CompileAndRunWithOneCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
